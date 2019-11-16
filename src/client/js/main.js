bc0cf7a81899e89da029825264091fc18c118ca1
fkropfhamer <fabian.kropfhamer@gmail.com>
2019-11-16 13:38:47 +0100

    fixed code styling

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,13 +1,22 @@
 import '../css/index.css';
 
-let socket;
+class Game {
+  constructor() {
+    this.setupSocket();
+  }
+
+  setupSocket() {
+    // eslint-disable-next-line no-undef
+    this.socket = io();
+    this.socket.on('connect', () => {
+      console.log('connected');
+    });
+  }
+}
 
 const init = () => {
-  // eslint-disable-next-line no-undef
-  socket = io();
-  socket.on('connect', () => {
-    console.log('connected');
-  });
+  // eslint-disable-next-line no-new
+  new Game();
 };
 
 window.onload = init();
