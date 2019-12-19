360ff506ec0e744a864b4ef882515a4cf8c27956
fkropfhamer <fabian.kropfhamer@sblum.de>
2019-12-19 13:30:40 +0100

    fixed tests and added mocks for assets

--- tests/client/view.test.js
+++ tests/client/view.test.js
@@ -29,10 +29,8 @@ describe('view', () => {
     // jest/jsdom defines the window innerWidth and innerHeight to be 1024 x 768
     expect(view.windowHeight).toBe(768);
     expect(view.windowWidth).toBe(1024);
-    expect(view.color).toBe('#232529');
     expect(view.scale).toBe(0.8);
     expect(view.height).toBe(576);
     expect(view.width).toBe(1024);
-    expect(view.canvas.style.backgroundColor).toBe('#232529');
   });
 });
