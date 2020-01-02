1700ac3786a9e699dbef583feef979bf308d5ac3
fkropfhamer <fabian.kropfhamer@sblum.de>
2020-01-02 13:46:45 +0100

    added test for view

--- tests/client/view.test.js
+++ tests/client/view.test.js
@@ -2,26 +2,26 @@ import View from '../../src/client/js/view';
 
 describe('view', () => {
   let view;
+  Object.defineProperty(document, 'getElementById', {
+    value: () => {
+      return { appendChild: jest.fn() };
+    },
+  });
+  Object.defineProperty(document, 'createElement', {
+    value: (element) => {
+      if (element === 'canvas') {
+        return {
+          getContext: () => {
+            return {};
+          },
+          style: {},
+        };
+      }
+      return { appendChild: jest.fn() };
+    },
+  });
 
   beforeEach(() => {
-    Object.defineProperty(document, 'getElementById', {
-      value: () => {
-        return { appendChild: jest.fn() };
-      },
-    });
-    Object.defineProperty(document, 'createElement', {
-      value: (element) => {
-        if (element === 'canvas') {
-          return {
-            getContext: () => {
-              return {};
-            },
-            style: {},
-          };
-        }
-        return { appendChild: jest.fn() };
-      },
-    });
     view = new View();
   });
 
@@ -32,5 +32,34 @@ describe('view', () => {
     expect(view.scale).toBe(0.8);
     expect(view.height).toBe(576);
     expect(view.width).toBe(1024);
+  });
+
+  test('view resize', () => {
+    view.setupCanvas = jest.fn();
+
+    view.resize();
+
+    expect(view.windowHeight).toBe(768 * 0.95);
+    expect(view.windowWidth).toBe(1024);
+    expect(view.setupCanvas).toHaveBeenCalledTimes(1);
+  });
+
+  test('view drawCircle', () => {
+    const mockContext = {
+      beginPath: jest.fn(),
+      arc: jest.fn(),
+      fill: jest.fn(),
+    };
+
+    view.ctx = mockContext;
+    view.scale = 1;
+
+    view.drawCircle(100, 100, 5, 'red');
+
+    expect(mockContext.beginPath).toHaveBeenCalledTimes(1);
+    expect(mockContext.arc).toHaveBeenCalledTimes(1);
+    expect(mockContext.arc).toHaveBeenCalledWith(100, 100, 5, 0, 2 * Math.PI, false);
+    expect(mockContext.fillStyle).toBe('red');
+    expect(mockContext.fill).toHaveBeenCalledTimes(1);
   });
 });
