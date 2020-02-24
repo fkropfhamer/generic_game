fcd64252a452404b6a059869e53fb9c82cb8ec23
fkropfhamer <fabian.kropfhamer@sblum.de>
2020-02-24 08:46:41 +0100

    removed console.log statements

--- src/server/server.js
+++ src/server/server.js
@@ -25,7 +25,6 @@ class Server {
 
   setupSocket() {
     this.io.on(SocketEvent.CONNECTION, (socket) => {
-      console.log('user connected');
       // eslint-disable-next-line no-new
       new Player(socket, this);
     });
@@ -52,7 +51,6 @@ class Server {
       if (!this.waitingPlayer) {
         this.waitingPlayer = player;
         player.notifyWaiting(1);
-        console.log('player is waiting');
       } else {
         const game = new Game([this.waitingPlayer, player]);
         game.start();
