8f70aa817ac12860112f20114d67f9e3ad188062
Christoph <c.bathon@campus.lmu.de>
2020-02-25 20:09:55 +0100

    fixed powerup placement function

--- src/server/game.js
+++ src/server/game.js
@@ -71,9 +71,16 @@ export default class Game {
 
   placeRandomPowerUp() {
     if (this.randomPowerUps.length < config.MAX_POWERUPS_ON_FIELD) {
-      const randomPowerUp = this.powerUps[Math.floor(Math.random() * this.powerUps.length)];
-      if (this.randomPowerUps.indexOf(randomPowerUp) === -1) {
-        this.randomPowerUps.push(randomPowerUp);
+      let randomPowerUp = this.powerUps[Math.floor(Math.random() * this.powerUps.length)];
+      let powerUpNotOnField = true;
+
+      while (powerUpNotOnField === true) {
+        if (this.randomPowerUps.indexOf(randomPowerUp) >= 0) {
+          randomPowerUp = this.powerUps[Math.floor(Math.random() * this.powerUps.length)];
+        } else if (this.randomPowerUps.indexOf(randomPowerUp) === -1) {
+          this.randomPowerUps.push(randomPowerUp);
+          powerUpNotOnField = false;
+        }
       }
     }
   }
